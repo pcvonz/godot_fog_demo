39c78a33029517b48a73ec19264e7447b60d6a24
pcvonz <pcvonz@gmail.com>
2019-02-06 20:34:53 -0800

    Cleanup code

--- dungeon.gd
+++ dungeon.gd
@@ -13,21 +13,19 @@ var map_position
 var vp
 var fog_vp
 
-onready var iso_to_cart = Transform2D().scaled(Vector2(1, 0.5)) * Transform2D.rotated(PI/4+PI/2) # Scale a transformation then rotate it (or maybe rotate then scale, not sure how it worked exactly..)
-onready var cart_to_iso = iso_to_cart.affine_inverse()
+onready var iso_to_cart = Transform2D().scaled(Vector2(1, .5)) * Transform2D.rotated(PI/4) # Scale a transformation then rotate it (or maybe rotate then scale, not sure how it worked exactly..)
+
 func _ready():
-	f = get_node('floor')
-	t = get_node("Node2D")
-	vp = get_viewport()
-	fog_vp = $Fog/Viewport
-	map_rect = f.get_used_rect()
-	map_size = map_rect.size * f.cell_size
-	fog_vp.size = map_size
-	map_position = iso_to_cart.xform_inv(f.map_to_world(map_rect.position))
-	iso_to_cart = iso_to_cart.translated(map_position)
-	t.get_node("TextureRect").rect_size = map_size
-	t.transform = iso_to_cart
-	$Fog.transform = iso_to_cart
+	var tile_bounds = calculate_bounds(get_node('floor'))
+	$Node2D/TextureRect.rect_size = tile_bounds.size
+	$Node2D.transform = iso_to_cart
+
+func calculate_bounds(tilemap):
+    var cell_bounds = tilemap.get_used_rect()
+    # create transform
+    var cell_to_pixel = Transform2D(Vector2(tilemap.cell_size.x * tilemap.scale.x, 0), Vector2(0, tilemap.cell_size.y * tilemap.scale.y), Vector2())
+    # apply transform
+    return Rect2(cell_to_pixel * cell_bounds.position, cell_to_pixel * cell_bounds.size)
 
 func _physics_process(delta):
 	get_node('Fog/Viewport/Node2D/Sprite').position = iso_to_cart.xform_inv($walls/troll.position)
